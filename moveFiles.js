cd0dc04c33cda14f7dfc13d5296bf1d72d553229
Stu Kennedy <info@continuata.com>
2020-09-18 13:10:33 +0100

    include binaries explicitly

---
+++ moveFiles.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+const appRootDir = require('app-root-dir').get();
+
+function setExecutable(path) {
+    const mode = fs.statSync(path).mode;
+    const executableMode = (mode | fs.constants.S_IXUSR); // set user executable bit
+    console.log("setting executable mode on " + path);
+    fs.chmodSync(path, executableMode);
+}
+
+const binSrcPath = path.join(__dirname, "bin");
+const binDestPath = path.join(appRootDir, "bin");
+const binDestPathMac = path.join(appRootDir, "bin", "darwin");
+const binDestPathWin = path.join(appRootDir, "bin", "win");
+
+fs.rename(binSrcPath, binDestPath, () => {
+  fs.readdir(binDestPathMac, (err, files) => {
+     if (err) throw err;
+     files.forEach(file => setExecutable(path.join(binDestPathMac, file)));
+  });
+  fs.readdir(binDestPathWin, (err, files) => {
+     if (err) throw err;
+     files.forEach(file => setExecutable(path.join(binDestPathWin, file)));
+  });
+});
