dfed85afdb1eed5eb743d601eba1382215111fda
Stu Kennedy <s.kennedy@waracle.com>
2023-03-03 10:55:33 +0000

    typescript improvements

--- index.d.ts
+++ index.d.ts
@@ -16,4 +16,16 @@ export interface Options {
  */
 export function uncompress(options: Options): Promise<'over'>;
 
-export function on(event: "progress", listener: (percent: number) => void): void;
+export function on(
+  event: 'progress',
+  listener: (percent: number) => void
+): void;
+
+export function on(
+  event: 'folders',
+  listener: (folders: string[]) => void
+): void;
+
+export function kill(): void;
+
+export type UnrarError = { code: string; archive: string };
